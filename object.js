6d29ace743079b292f8e718f37d9d096d7cd2df3
Bondaruk Liudmyla <ludabond123456@gmail.com>
2023-03-14 18:29:15 +0100

    Odject

---
+++ object.js
@@ -0,0 +1,3 @@
+"use strict";
+durationBetweenDates('02 Aug 1985', '03 Aug 1985', 'seconds') // поверне '86400 seconds'
+durationBetweenDates('31 Jan 2022', '03 Feb 2021', 'days') // поверне '362 days
