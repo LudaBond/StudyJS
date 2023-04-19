2cb69ed88ae807f8d6c7b49da63d7d910fe2607f
Bondaruk Liudmyla <ludabond123456@gmail.com>
2023-04-19 22:34:26 +0200

    calc4

--- calc1.js
+++ calc1.js
@@ -5,120 +5,106 @@ const measureSelect = document.getElementById("measure");
 const resultDiv = document.getElementById("results");
 const calculateBtn = document.getElementById("calculate-btn");
 const daysOptionSelect = document.getElementById("daysOption");
+const periodSelect = document.getElementById("period");
 
+endDateInput.disabled = true;
+startDateInput.addEventListener("input", () => {
+    endDateInput.disabled = false;
+    endDateInput.min = startDateInput.value;
+});
+periodSelect.addEventListener("change", () => {
+    const startDate = new Date(startDateInput.value);
+    let endDate;
+    if (periodSelect.value === "week") {
+        endDate = new Date(startDate.getTime() + 7 * 24 * 60 * 60 * 1000);
+    } else if (periodSelect.value === "month") {
+        endDate = new Date(startDate.getTime() + 30 * 24 * 60 * 60 * 1000);
+    }
+    document.getElementById("end-date").value = endDate.toISOString().slice(0, 10);
+});
 calculateBtn.addEventListener("click", () => {
     const startDate = new Date(startDateInput.value);
     const endDate = new Date(endDateInput.value);
     const measure = measureSelect.value;
     const daysOption = daysOptionSelect.value;
-
-    const diff = calculateDateDifference(startDate, endDate, measure);
-    // const option = countDays(startDate, endDate, daysOption);
-
+    const period = periodSelect.value;
+    const diff = calculateDateDifference(startDate, endDate, daysOption, measure);
     const row = document.createElement("tr");
-
     const td1 = document.createElement("td");
     const td2 = document.createElement("td");
     const td3 = document.createElement("td");
     const td4 = document.createElement("td");
+    const td5 = document.createElement("td");
 
-    td1.innerText = `${startDate}`
-    td2.innerText = `${endDate}`
-    td3.innerText = `${measure}`
+    td1.innerText = `${startDate.toLocaleDateString()}`
+    td2.innerText = `${endDate.toLocaleDateString()}`
+    td3.innerText = `${daysOption}`
     td4.innerText = `${diff}`
+    td5.innerText = `${measure}`
 
     row.appendChild(td1);
-    row.appendChild(td2)
-    row.appendChild(td3)
-    row.appendChild(td4)
+    row.appendChild(td2);
+    row.appendChild(td3);
+    row.appendChild(td4);
+    row.appendChild(td5);
 
-    resultDiv.appendChild(row);
+    resultDiv.insertBefore(row, resultDiv.firstChild);
+    addToHistory(startDate.toLocaleDateString(), endDate.toLocaleDateString(), daysOption, diff, measure);
 
-    addToHistory(startDate, endDate, measure, diff);
-    // resultDiv.innerText = ` ${option}. `;
+    if (resultDiv.children.length > 9) {
+        resultDiv.removeChild(resultDiv.lastChild);
+    }
+    function calculateDateDifference(startDate, endDate, daysOption, measure) {
+        const oneDay = 24 * 60 * 60 * 1000;
+        const daysBetween = Math.round(Math.abs((startDate.getTime() - endDate.getTime()) / oneDay) + 1);
+        let countDays = 0;
+        if (daysOption === 'allDays') {
+            countDays = daysBetween;
+        } else {
+            const weekends = [0, 6];
+            for (let i = 0; i < daysBetween; i++) {
+                const date = new Date(startDate.getTime() + (i * oneDay));
+                const dayOfWeek = date.getDay();
 
-
-    function calculateDateDifference(startDate, endDate, measure) {
-        const diffInMs = endDate - startDate + 24 * 60 * 60 * 1000;
-
-        switch (measure) {
-            case "days":
-                return Math.floor(diffInMs / (1000 * 60 * 60 * 24));
-            case "hours":
-                return Math.floor(diffInMs / (1000 * 60 * 60));
-            case "minutes":
-                return Math.floor(diffInMs / (1000 * 60));
-            case "seconds":
-                return Math.floor(diffInMs / 1000);
-            default:
-                throw new Error("Invalid measure value");
-        };
-        return days;
-    };
-    function countDays(startDate, endDate, daysOption) {
-        const oneDay = 24 * 60 * 60 * 1000; // Кількість мілісекунд у одному дні
-        const daysBetween = Math.round(Math.abs((startDate.getTime() - endDate.getTime()) / oneDay)) + 1;
-        const weekends = [0, 6]; // Номери днів, які вважаються вихідними (неділя та субота)
-
-        let countWeekdays = 0;
-        let countWeekends = 0;
-
-        // Проходимо по кожному дню між датами
-        for (let i = 0; i < daysBetween; i++) {
-            const date = new Date(startDate.getTime() + (i * oneDay));
-            const dayOfWeek = date.getDay();
-
-            // Якщо день належить до вихідних
-            if (weekends.includes(dayOfWeek)) {
-                countWeekends++;
-            } else {
-                countWeekdays++;
+                if ((daysOption === 'Weekdays' && !weekends.includes(dayOfWeek)) ||
+                    (daysOption === 'Weekends' && weekends.includes(dayOfWeek))) {
+                    countDays++;
+                }
             }
         }
-        if (daysOption === 'all') {
-            console.log(`Загальна кількість днів між ${startDate.toDateString()} та ${endDate.toDateString()}: ${daysBetween}`);
-        } else if (daysOption === 'weekends') {
-            console.log(`Кількість вихідних днів між ${startDate.toDateString()} та ${endDate.toDateString()}: ${countWeekends}`);
-        } else if (daysOption === 'weekdays') {
-            console.log(`Кількість будніх днів між ${startDate.toDateString()} та ${endDate.toDateString()}: ${countWeekdays}`);
+        switch (measure) {
+            case "days":
+                return countDays;
+            case "hours":
+                return countDays * 24;
+            case "minutes":
+                return countDays * 24 * 60;
+            case "seconds":
+                return countDays * 24 * 60 * 60;
+            default:
+                throw new Error("Invalid measure value");
         }
-    }
-    countDays(startDate, endDate, 'all');
-    countDays(startDate, endDate, 'weekends');
-    countDays(startDate, endDate, 'weekdays');
-
+    };
 });
-
-
-function addToHistory(startDate, endDate, measure, diff) {
-    // Отримуємо історію результатів з локального сховища
+function addToHistory(startDate, endDate, daysOption, diff, measure) {
     let history = JSON.parse(localStorage.getItem("history")) || [];
-
-    // Додаємо новий результат до історії
     history.push({
         startDate: startDate,
         endDate: endDate,
+        daysOption: daysOption,
         measure: measure,
         diff: diff
     });
-
-    // Якщо історія містить більше 10 результатів, видаляємо найстаріший
-    if (history.length > 10) {
+    if (history.length > 9) {
         history.shift();
-        // resultDiv як видалити останню дитину в дом елемененту
     }
-
-    // Зберігаємо оновлену історію в локальному сховищі
     localStorage.setItem("history", JSON.stringify(history));
 }
-// addToHistory();
-
-
 let stringifiedHistory = JSON.parse(localStorage.getItem("history")) || [];
 
 if (history.length) {
 
-    stringifiedHistory.forEach(({ startDate, endDate, measure, diff }) => {
+    stringifiedHistory.forEach(({ startDate, endDate, daysOption, diff, measure }) => {
         console.log
 
         const row = document.createElement("tr");
@@ -126,75 +112,20 @@ if (history.length) {
         const td2 = document.createElement("td");
         const td3 = document.createElement("td");
         const td4 = document.createElement("td");
+        const td5 = document.createElement("td");
+
 
         td1.innerText = `${startDate}`
         td2.innerText = `${endDate}`
-        td3.innerText = `${measure}`
+        td3.innerText = `${daysOption}`
         td4.innerText = `${diff}`
+        td5.innerText = `${measure}`
 
         row.appendChild(td1);
         row.appendChild(td2)
         row.appendChild(td3)
         row.appendChild(td4)
-
+        row.appendChild(td5)
         resultDiv.appendChild(row);
     });
 }
-
-
-// function addToHistory(startDate, endDate, measure, diff) {
-//     // Отримуємо історію результатів з локального сховища
-//     let history = JSON.parse(localStorage.getItem("history")) || [];
-
-//     // Додаємо новий результат до історії
-//     history.push({
-//         start: startDate,
-//         end: endDate,
-//         measure: measure,
-//         diff: diff
-//     });
-
-//     // Якщо історія містить більше 10 результатів, видаляємо найстаріший
-//     if (history.length > 10) {
-//         history.shift();
-//     }
-
-//     // Зберігаємо оновлену історію в локальному сховищі
-//     localStorage.setItem("history", JSON.stringify(history));
-
-// }
-// addToHistory();
-
-
-    // function addResultToTable() {
-    //     // Отримуємо значення з полів введення
-    //     let input1 = parseFloat(document.getElementById("input1").value);
-    //     let input2 = parseFloat(document.getElementById("input2").value);
-
-    //     // Рахуємо результат
-    //     let result = input1 + input2;
-
-    //     // Отримуємо таблицю за її id
-    //     let table = document.getElementById("results-table");
-
-    //     // Створюємо новий рядок
-    //     let newRow = table.insertRow();
-
-    //     // Створюємо три нові комірки для нашого нового рядка
-    //     let cell1 = newRow.insertCell();
-    //     let cell2 = newRow.insertCell();
-    //     let cell3 = newRow.insertCell();
-
-    //     // Заповнюємо комірки значеннями
-    //     cell1.innerHTML = input1;
-    //     cell2.innerHTML = input2;
-    //     cell3.innerHTML = result;
-    // };
-
-
-
-
-
-
-
-
